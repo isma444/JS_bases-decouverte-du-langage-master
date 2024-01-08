fc51497cd9e19c1b3d2240ab91b2d90ff1c03c33
ismael vas <155621485+isma444@users.noreply.github.com>
2024-01-08 10:40:19 +0100

    ## Ex 1 - Les variables

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,7 @@
 // you can write js here
 console.log('hello from file');
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?")
+// let  kelvins = 294;
+let celsius = kelvins - 273;
+let fahrenheits = Math.floor(celsius * (9/5) + 32); // Je déclare la variable nommé farhenheights qui prend pour valeur le resultat de l'equation celsius * (9/5) + 32 (celsius représente la valeur de la variable celsius), arrondi a l'entier le plus proche
+ console.log("celisus : " + celsius + " fahrenheits : " + fahrenheits );
