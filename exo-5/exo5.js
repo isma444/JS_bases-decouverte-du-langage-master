8ad3d393bb037ce9d44845d9449b39e2537e40f0
ismael vas <155621485+isma444@users.noreply.github.com>
2024-01-08 15:03:19 +0100

    ## Ex 5 - Les boucles

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,26 @@
 // you can write js here
 
 console.log('exo-5');
+let input = "phrase de test";
+let letters = input.split("");
+let vowels =  ["a", "e", "i", "o", "u", "y"];
+let resultArray = [];
+
+// letters.forEach(elmt=> {
+//     if(vowels.indexOf(elmt) >= 0){
+//         resultArray.push(elmt)
+//     } 
+    
+// });
+
+letters.forEach(elmt=> {
+    vowels.forEach(e =>{
+        if(elmt == e){
+            resultArray.push(elmt);
+        }
+    })
+    
+});
+
+console.log(resultArray.join("").toLocaleUpperCase());
+
