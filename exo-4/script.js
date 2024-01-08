b13e0876c5032805858b4abf95df177d53b4551d
ismael vas <155621485+isma444@users.noreply.github.com>
2024-01-08 14:36:56 +0100

    ## Ex 4 - Les tableaux

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,15 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+secretMessage.pop();
+secretMessage.push("to");
+secretMessage.push("program");
+const isEasily = (element) => element == "easily";
+let easilyKey = secretMessage.findIndex(isEasily);
+secretMessage[easilyKey] = "right";
+secretMessage.shift();
+secretMessage.unshift("Programming")
+secretMessage.splice(5,5 ,"know")
+
+console.log(secretMessage.join(' '));
